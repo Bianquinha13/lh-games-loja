ab488fcb4a05d7112736f95ce4c00c4862870eb3
Bianquinha13 <mariabiancavitoria10@gmail.com>
2023-03-28 15:49:35 -0300

    atividade 2

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component } from'@angular/core';
+import { LoginService } from '../login.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  usuario!: string;
+  senha!: string;
 
+  constructor(private _loginservice:LoginService, private _router: Router){}
+
+  fazerLogin(){
+    this._loginservice.login(this.usuario,this.senha);
+    this._router.navigate(['/restrito/lista']);
+    this._loginservice.setMostraMenu(false);
+  }
 }
